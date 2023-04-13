1c2dba49dcc3aa60233016eb6d961a03cebf0fa0
kzhang425 <kzhang425@gmail.com>
2023-04-13 13:38:30 -0400

    Bug fixes

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 public class Program
 {
+	// The only purpose of this script is to test it as a standalone. DO NOT use this script in Unity as it will likely do nothing
 	public static void Main()
 	{
 
